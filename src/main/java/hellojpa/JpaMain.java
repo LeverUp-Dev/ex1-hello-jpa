94f14278b36da34aa13f7e96871efca980d326d4
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2024-02-03 07:59:59 +0900

    기본 키 매핑_@GeneratedValue(strategy = GenerationType.SEQUENCE)
    데이터베이스 SEQUENCE 는 유일한 값을 순서대로 생성하는 특별한
    데이터베이스 오브젝트(예: 오라클 시퀀스)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,8 +2,6 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
-import java.util.List;
-
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,11 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
 @Getter @Setter
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
     private Long id;
 
     @Column(name = "name", nullable = false)
