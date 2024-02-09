a3fa8ab649ab08f0fbe9952cb2da08e3425f7574
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2024-02-09 23:50:18 +0900

    프록스_02
    프록시 객체는 원본 엔티티를 상속받음, 따라서 타입 체크시 주의해야함 (== 비교 실패, 대신 instance of 사용)
    
    실무에서는 프록시를 쓸지 진짜 엔티티 객체를 쓸지 알 수 없기에  Instance of를 써서 비교해야 한다.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,19 +13,21 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("hello");
-
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            em.persist(member1);
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            //
-//            Member findMember = em.find(Member.class, member.getId());
-            Member findMember = em.getReference(Member.class, member.getId());
-            System.out.println("findMember.username = " + findMember.getUsername());
-            System.out.println("findMember.username = " + findMember.getUsername());
+            Member m1 = em.find(Member.class, member1.getId());
+            Member m2 = em.getReference(Member.class, member2.getId());
+
+            //logic(m1, m2);
+            logic2(m1, m2);
 
             tx.commit();
         } catch (Exception e) {
@@ -35,5 +37,14 @@ public class JpaMain {
         }
 
         emf.close();
+    }
+
+    private static void logic(Member m1, Member m2) {
+        System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass()));
+    }
+
+    private static void logic2(Member m1, Member m2) {
+        System.out.println("m1 == m2 : " + (m1 instanceof Member));
+        System.out.println("m1 == m2 : " + (m2 instanceof Member));
     }
 }
