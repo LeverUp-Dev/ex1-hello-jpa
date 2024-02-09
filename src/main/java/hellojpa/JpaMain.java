26fee0fcbc1da222ac894e1a2ec458a0b19448cd
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2024-02-10 00:01:13 +0900

    프록시_03
    영속성 컨텍스트에 찾는 엔티티가 이미 있으면 em.getReference()를 호출해도 실제 엔티티 반환한다.
    
    같은 pk값을 가지고 있으면 참조값이 아닌 진짜 객체를 반환한다.
    예)
     Member m1 = em.find(Member.class, member1.getId());
                System.out.println("m1 = " + m1.getClass());
    
                Member reference = em.getReference(Member.class, member1.getId());
                System.out.println("reference = " + reference.getClass());

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,18 +16,15 @@ public class JpaMain {
             Member member1 = new Member();
             member1.setUsername("member1");
             em.persist(member1);
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            em.persist(member2);
 
             em.flush();
             em.clear();
 
             Member m1 = em.find(Member.class, member1.getId());
-            Member m2 = em.getReference(Member.class, member2.getId());
+            System.out.println("m1 = " + m1.getClass());
 
-            //logic(m1, m2);
-            logic2(m1, m2);
+            Member reference = em.getReference(Member.class, member1.getId());
+            System.out.println("reference = " + reference.getClass());
 
             tx.commit();
         } catch (Exception e) {
@@ -37,14 +34,5 @@ public class JpaMain {
         }
 
         emf.close();
-    }
-
-    private static void logic(Member m1, Member m2) {
-        System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass()));
-    }
-
-    private static void logic2(Member m1, Member m2) {
-        System.out.println("m1 == m2 : " + (m1 instanceof Member));
-        System.out.println("m1 == m2 : " + (m2 instanceof Member));
     }
 }
