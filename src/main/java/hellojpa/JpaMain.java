54bf7ce884c02616e3900c641d6cbd138062b54f
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2024-02-14 01:20:26 +0900

    값_타입_컬렉션-수정시 주의점
    값 타입 컬렉션은 변경 시 주인엔티티와 관련괸 모든 데이터를 삭제하기 때문에 주의 해야한다.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -31,22 +31,20 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            System.out.println("------------------------------------");
+            System.out.println("------------START-------------");
             Member findMember = em.find(Member.class, member.getId());
-
-            //목표: homeCity -> newCity
-//            findMember.getHomeAddress().setCity("newCity"); 잘못된 예시
-            Address a = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            //목표: 치킨 -> 한식
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
+//
+//            //목표: homeCity -> newCity
+////            findMember.getHomeAddress().setCity("newCity"); 잘못된 예시
+//            Address a = findMember.getHomeAddress();
+//            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
+//
+//            //목표: 치킨 -> 한식
+//            findMember.getFavoriteFoods().remove("치킨");
+//            findMember.getFavoriteFoods().add("한식");
 
             findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
             findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
-
-            System.out.println("------------------------------------");
 
             tx.commit();
         } catch (Exception e) {
